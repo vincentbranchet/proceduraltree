3d5ea33b9573574a44122caa98e4bdb144a42d4b
Vincent Branchet <vince.branchet@gmail.com>
2023-06-25 12:29:45 +0200

    Added proper create flow

--- back/index.js
+++ back/index.js
@@ -1,11 +1,8 @@
 import Fastify from "fastify"
-import path from "path"
-import { fileURLToPath } from 'url';
-import fastifyStatic from "@fastify/static"
 import fastifyMysql from "@fastify/mysql"
-import crypto from "crypto"
-import fs from "fs"
 import fastifyEnv from "@fastify/env"
+import fastifyFormbody from "@fastify/formbody"
+import routes from "./routes.js"
 
 const server = new Fastify({ logger: true })
 
@@ -26,73 +23,11 @@ const env = {
 const start = async () => {
     await server.register(fastifyEnv, env)
 
-    server.register(fastifyStatic, {
-        root: path.join(path.dirname(fileURLToPath(import.meta.url)), '../public'),
-        prefix: '/public/'
-    })
     server.register(fastifyMysql, {
         connectionString: process.env.DB_URL
     })
-
-    server.get('/', (request, reply) => {
-        reply.sendFile('index.html')
-    })
-
-    server.get('/create', {
-        schema: {
-            query: {
-                type: 'object',
-                properties: {
-                    name: { type: 'string' },
-                    planted: { type: 'string' }
-                },
-                required: ['name', 'planted']
-
-            }
-        }
-    }, (request, reply) => {
-        const toHash = request.query.name + request.query.planted
-        const hash = crypto.createHash('md5').update(toHash).digest('hex')
-        const datetime = new Date(request.query.planted).toISOString().slice(0, 19).replace('T', ' ')
-
-        server.mysql.query(
-            'INSERT INTO seeds ( name, planted, hash ) VALUES (?, ?, ?)',
-            [request.query.name, datetime, hash],
-            (err, result) => {
-                reply.send(err || result)
-            }
-        )
-    })
-
-    server.get('/config', (request, reply) => {
-        const config = fs.readFileSync('src/config.json')
-
-        reply.send(config)
-    })
-
-    server.get('/:hash', (request, reply) => {
-
-        server.mysql.query(
-            'SELECT * FROM seeds WHERE hash = ?',
-            [request.params.hash],
-            (err, result) => {
-                if (result.length === 1) {
-                    try {
-                        const js = fs.readFileSync('front/build/index.js')
-                        const html = fs.readFileSync('front/index.html')
-                        const config = fs.readFileSync('back/config.json')
-
-                        const clientJs = js.toString().replace('PLACEHOLDER_NAME', result[0].name).replace('PLACEHOLDER_BIRTHDAY', result[0].planted).replace('\"PLACEHOLDER_CONFIG\"', config)
-                        const clientHtml = html.toString().replace('PLACEHOLDER_APP_CODE', clientJs).replace('PLACEHOLDER_TREE_NAME', result[0].name)
-
-                        reply.header('Content-Type', 'text/html').send(clientHtml)
-                    } catch (err) {
-                        reply.send(err)
-                    }
-                }
-            }
-        )
-    })
+    server.register(fastifyFormbody)
+    server.register(routes)
 
     server.listen({ port: 3000 }, (err, address) => {
         if (err) {
---
+++ back/routes.js
@@ -0,0 +1,66 @@
+import crypto from "crypto"
+import fs from "fs"
+
+export default async function routes(server, options) {
+    // INDEX
+    server.get('/', (request, reply) => {
+        const html = fs.readFileSync('front/views/create.html')
+        reply.header('Content-Type', 'text/html').send(html)
+    })
+
+    // CREATE
+    server.post('/create', {
+        schema: {
+            body: {
+                type: 'object',
+                properties: {
+                    name: { type: 'string' },
+                    date: { type: 'string' }
+                },
+                required: ['name', 'date']
+            }
+        }
+    }, (request, reply) => {
+        const toHash = request.body.name + request.body.date
+        const hash = crypto.createHash('md5').update(toHash).digest('hex')
+        const datetime = new Date(request.body.date).toISOString().slice(0, 19).replace('T', ' ')
+
+        server.mysql.query(
+            'INSERT INTO seeds ( name, planted, hash ) VALUES (?, ?, ?)',
+            [request.body.name, datetime, hash],
+            (err, result) => {
+                if (err)
+                    reply.send(err)
+                else {
+                    reply.redirect('/' + hash)
+                }
+            }
+        )
+    })
+
+    // SHOW
+    server.get('/:hash', (request, reply) => {
+
+        server.mysql.query(
+            'SELECT * FROM seeds WHERE hash = ?',
+            [request.params.hash],
+            (err, result) => {
+                if (result.length === 1) {
+                    try {
+                        const js = fs.readFileSync('front/build/index.js')
+                        const html = fs.readFileSync('front/views/index.html')
+                        const config = fs.readFileSync('back/config.json')
+
+                        const clientJs = js.toString().replace('PLACEHOLDER_NAME', result[0].name).replace('PLACEHOLDER_BIRTHDAY', result[0].planted).replace('\"PLACEHOLDER_CONFIG\"', config)
+                        const clientHtml = html.toString().replace('PLACEHOLDER_APP_CODE', clientJs).replace('PLACEHOLDER_TREE_NAME', result[0].name)
+
+                        reply.header('Content-Type', 'text/html').send(clientHtml)
+                    } catch (err) {
+                        reply.send(err)
+                    }
+                }
+            }
+        )
+    })
+
+}
