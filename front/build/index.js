47dbb7d7282bdb852402949ea3a9f6e9b4096f38
Vincent Branchet <vince.branchet@gmail.com>
2023-12-06 08:47:00 +0100

    Split big logic blocks into smaller ones

--- front/build/index.js
+++ front/build/index.js
@@ -83,103 +83,34 @@
       this.animationPipe = [];
       this.blueprint = [];
       this.drawn = { value: 0 };
-      this.canvas = document.createElement("canvas");
-      this.context = this.canvas.getContext("2d");
-      this.container = document.getElementById("main-container");
       this.windResistance = 5;
-      this.info = document.getElementById("info-text");
       this.lastFrameTime = null;
       this.buildId = 0;
       this.fps = 12;
     }
     start() {
-      this.buildCanvas();
+      this.renderCanvas();
       this.fillBackground();
-      this.writeInfo();
-      this.root = { x: this.canvas.width / 2, y: this.canvas.height };
-      this.pipe.push({ id: this.buildId, prevId: 0, x: this.root.x, y: this.root.y, length: 0, angle: -Math.PI / 2, depth: 0, thickness: config.branchThickness });
-      while (this.pipe.length > 0 && this.drawn.value <= config.age) {
-        const next = this.pipe[0];
-        this.build(next.x, next.y, next.angle, next.depth, next.thickness, next.id, next.prevId);
-        this.drawn.value++;
-        this.pipe.shift();
-      }
-      window.requestAnimationFrame(this.animate.bind(this));
+      this.fillInfoPanel();
+      this.buildBlueprint();
+      this.animate();
     }
-    build(x, y, angle, depth, thickness, id, prevId) {
-      const endX = x + Math.cos(angle) * (config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength);
-      const endY = y + Math.sin(angle) * (config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength);
-      const length = Math.sqrt(Math.pow(endX - x, 2) + Math.pow(endY - y, 2));
-      const leafDice = config.seedRandom(x) - (depth - config.maxDepth) / 10;
-      this.blueprint.push({
-        id,
-        prevId,
-        depth,
-        startNode: { x, y },
-        endNode: { x: endX, y: endY },
-        color: colorWithVariation(config.branchColor, config.branchColorVariation, endX, config.seedRandom),
-        length,
-        thickness,
-        angle,
-        leaf: { dice: leafDice, ellipse: config.seedRandom(y) * 360, color: colorWithVariation(config.leafColor, config.leafColorVariation, x, config.seedRandom) }
-      });
-      const d1 = getCurve(endX, endY, angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom);
-      const d2 = getCurve(endX + 1, endY + 1, angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom);
-      const thickness1 = getThickness(endY, thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom);
-      const thickness2 = getThickness(endY, thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom);
-      this.buildId++;
-      this.pipe.push({ id: this.buildId, prevId: id, x: endX, y: endY, angle: angle + config.branchAngleVariation, length, depth: depth + 1, thickness: thickness1, ccpX: d1.x, ccpY: d1.y, leafDice });
-      this.buildId++;
-      this.pipe.push({ id: this.buildId, prevId: id, x: endX, y: endY, angle: angle - config.branchAngleVariation, length, depth: depth + 1, thickness: thickness2, ccpX: d2.x, ccpY: d2.y, leafDice });
-    }
-    animate() {
-      const now = window.performance.now();
-      const delta = now - this.lastFrameTime;
-      if (delta > 1e3 / this.fps) {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        const angleVariation = Math.random() * 1 / this.windResistance;
-        let pipe = [];
-        let framePipe = [];
-        pipe.push(this.blueprint[0]);
-        while (pipe.length > 0) {
-          const current = pipe[0];
-          const endX = current.startNode.x + current.length * Math.cos(current.angle + angleVariation * current.depth / 10);
-          const endY = current.startNode.y + current.length * Math.sin(current.angle + angleVariation * current.depth / 10);
-          const previousBranch = framePipe.find((branch) => branch.id === current.prevId);
-          framePipe.push({
-            x: previousBranch ? previousBranch.endX : this.root.x,
-            y: previousBranch ? previousBranch.endY : this.root.y,
-            endX,
-            endY,
-            color: current.color,
-            thickness: current.thickness,
-            leaf: current.leaf,
-            id: current.id
-          });
-          const next = this.blueprint.find((branch) => branch.id === current.id + 1);
-          if (next) {
-            pipe.push(next);
-          }
-          pipe.shift();
-        }
-        for (const branch of framePipe) {
-          this.draw(branch.x, branch.y, branch.endX, branch.endY, branch.color, branch.thickness, branch.leaf);
-        }
-        this.lastFrameTime = now;
-      }
-      window.requestAnimationFrame(this.animate.bind(this));
-    }
-    buildCanvas() {
+    renderCanvas() {
+      this.canvas = document.createElement("canvas");
       this.canvas.width = config.canvasWidth;
       this.canvas.height = config.canvasHeight;
+      this.container = document.getElementById("main-container");
       this.container.appendChild(this.canvas);
-      this.context.fillStyle = config.skyColor;
-      this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      this.root = { x: this.canvas.width / 2, y: this.canvas.height };
     }
     fillBackground() {
+      this.context = this.canvas.getContext("2d");
+      this.context.fillStyle = config.skyColor;
+      this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
       this.container.style.backgroundColor = config.skyColor;
     }
-    writeInfo() {
+    fillInfoPanel() {
+      this.info = document.getElementById("info-text");
       this.info.innerHTML = `Cet arbre s'appelle ${config.seed.name}. Il pousse depuis ${config.age} jours.
 
 Pour le partager et le retrouver facilement, copiez le lien de cette page depuis la barre d'adresse de votre navigateur, ou ci-dessous :
@@ -188,6 +119,113 @@ ${window.location.href}
 
 
 L'acc\xE8s \xE0 cet arbre est libre et gratuit pour tout le monde, et le restera pour toujours.`;
+    }
+    buildBlueprint() {
+      this.pipe.push({
+        id: this.buildId,
+        prevId: 0,
+        x: this.root.x,
+        y: this.root.y,
+        angle: -Math.PI / 2,
+        depth: 0,
+        thickness: config.branchThickness,
+        length: 0
+      });
+      while (this.drawn.value <= config.age) {
+        const current = this.pipe[0];
+        const branch = this.buildBranch(current);
+        this.blueprint.push(branch);
+        this.getNextInPipe(branch);
+        this.drawn.value++;
+        this.pipe.shift();
+      }
+    }
+    buildBranch({ id, prevId, x, y, angle, depth, thickness }) {
+      const { nextX, nextY } = this.nextCoordinates(x, y, angle);
+      const length = Math.sqrt(Math.pow(nextX - x, 2) + Math.pow(nextY - y, 2));
+      const leafDice = config.seedRandom(x) - (depth - config.maxDepth) / 10;
+      return {
+        id,
+        prevId,
+        depth,
+        startNode: { x, y },
+        endNode: { x: nextX, y: nextY },
+        color: colorWithVariation(config.branchColor, config.branchColorVariation, nextX, config.seedRandom),
+        length,
+        thickness,
+        angle,
+        leaf: { dice: leafDice, ellipse: config.seedRandom(y) * 360, color: colorWithVariation(config.leafColor, config.leafColorVariation, x, config.seedRandom) }
+      };
+    }
+    nextCoordinates(x, y, angle) {
+      const nextX = x + Math.cos(angle) * (config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength);
+      const nextY = y + Math.sin(angle) * (config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength);
+      return { nextX, nextY };
+    }
+    getNextInPipe(branch) {
+      const d1 = getCurve(branch.endNode.x, branch.endNode.y, branch.angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom);
+      const d2 = getCurve(branch.endNode.x + 1, branch.endNode.y + 1, branch.angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom);
+      const thickness1 = getThickness(branch.endNode.y, branch.thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom);
+      const thickness2 = getThickness(branch.endNode.y, branch.thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom);
+      this.pipe = this.pipe.concat(this.getNextInBlueprint(branch, d1, d2, thickness1, thickness2));
+    }
+    getNextInBlueprint(branch, d1, d2, thickness1, thickness2) {
+      this.buildId++;
+      const firstNext = { id: this.buildId, prevId: branch.id, x: branch.endNode.x, y: branch.endNode.y, angle: branch.angle + config.branchAngleVariation, length: branch.length, depth: branch.depth + 1, thickness: thickness1, ccpX: d1.x, ccpY: d1.y, leafDice: branch.leaf.dice };
+      this.buildId++;
+      const secondNext = { id: this.buildId, prevId: branch.id, x: branch.endNode.x, y: branch.endNode.y, angle: branch.angle - config.branchAngleVariation, length: branch.length, depth: branch.depth + 1, thickness: thickness2, ccpX: d2.x, ccpY: d2.y, leafDice: branch.leaf.dice };
+      return [firstNext, secondNext];
+    }
+    animate() {
+      window.requestAnimationFrame(this.drawAnimationFrame.bind(this));
+    }
+    drawAnimationFrame() {
+      const now = window.performance.now();
+      const delta = now - this.lastFrameTime;
+      if (delta > 1e3 / this.fps) {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        const angleVariation = Math.random() * 1 / this.windResistance;
+        const tree = this.buildAnimationTree(angleVariation);
+        for (const branch of tree) {
+          this.draw(branch.x, branch.y, branch.endX, branch.endY, branch.color, branch.thickness, branch.leaf);
+        }
+        this.lastFrameTime = now;
+      }
+      window.requestAnimationFrame(this.animate.bind(this));
+    }
+    buildAnimationTree(angleVariation) {
+      let pipe = [];
+      let branches = [];
+      pipe.push(this.blueprint[0]);
+      while (pipe.length > 0) {
+        const current = pipe[0];
+        const { endX, endY } = this.nextAnimatedCoordinates(current, angleVariation);
+        const previous = branches.find((branch) => branch.id === current.prevId);
+        branches.push(this.getAnimationBranch(previous, current, endX, endY));
+        const next = this.blueprint.find((branch) => branch.id === current.id + 1);
+        if (next) {
+          pipe.push(next);
+        }
+        pipe.shift();
+      }
+      return branches;
+    }
+    nextAnimatedCoordinates(blueprint, angleVariation) {
+      const endX = blueprint.startNode.x + blueprint.length * Math.cos(blueprint.angle + angleVariation * blueprint.depth / 10);
+      const endY = blueprint.startNode.y + blueprint.length * Math.sin(blueprint.angle + angleVariation * blueprint.depth / 10);
+      return { endX, endY };
+    }
+    getAnimationBranch(previous, current, endX, endY) {
+      return {
+        x: previous ? previous.endX : this.root.x,
+        y: previous ? previous.endY : this.root.y,
+        endX,
+        endY,
+        color: current.color,
+        thickness: current.thickness,
+        leaf: current.leaf,
+        id: current.id
+      };
     }
     draw(x, y, endX, endY, color, thickness, leaf, curveControlPointX = x, curveControlPointY = y) {
       this.context.beginPath();
--- front/models/app.js
+++ front/models/app.js
@@ -7,124 +7,132 @@ export class App {
         this.animationPipe = []
         this.blueprint = []
         this.drawn = { value: 0 }
-        this.canvas = document.createElement('canvas')
-        this.context = this.canvas.getContext('2d')
-        this.container = document.getElementById('main-container')
         this.windResistance = 5
-        this.info = document.getElementById('info-text')
         this.lastFrameTime = null
         this.buildId = 0
         this.fps = 12 // TODO : browser crashes close to 24
     }
 
     start() {
-        this.buildCanvas()
+        this.renderCanvas()
         this.fillBackground()
-        this.writeInfo()
+        this.fillInfoPanel()
+        this.buildBlueprint()
+        this.animate()
+    }
+
+    renderCanvas() {
+        this.canvas = document.createElement('canvas')
+        this.canvas.width = config.canvasWidth
+        this.canvas.height = config.canvasHeight
+        this.container = document.getElementById('main-container')
+        this.container.appendChild(this.canvas)
+
         this.root = { x: this.canvas.width / 2, y: this.canvas.height }
+    }
 
-        this.pipe.push({ id: this.buildId, prevId: 0, x: this.root.x, y: this.root.y, length: 0, angle: -Math.PI / 2, depth: 0, thickness: config.branchThickness });
+    fillBackground() {
+        this.context = this.canvas.getContext('2d')
+        this.context.fillStyle = config.skyColor
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        this.container.style.backgroundColor = config.skyColor
+    }
 
-        // enregistrer la structure de l'arbre dans le pipe
-        while (this.pipe.length > 0 && this.drawn.value <= config.age) {
-            const next = this.pipe[0]
+    fillInfoPanel() {
+        this.info = document.getElementById('info-text')
+        this.info.innerHTML = `Cet arbre s'appelle ${config.seed.name}. Il pousse depuis ${config.age} jours.\n
+Pour le partager et le retrouver facilement, copiez le lien de cette page depuis la barre d'adresse de votre navigateur, ou ci-dessous :\n
+${window.location.href} \n\n
+L'accès à cet arbre est libre et gratuit pour tout le monde, et le restera pour toujours.`
+    }
 
-            this.build(next.x, next.y, next.angle, next.depth, next.thickness, next.id, next.prevId)
+    buildBlueprint() {
+        this.pipe.push({
+            id: this.buildId,
+            prevId: 0,
+            x: this.root.x,
+            y: this.root.y,
+            angle: -Math.PI / 2,
+            depth: 0,
+            thickness: config.branchThickness,
+            length: 0,
+        })
+
+        while (this.drawn.value <= config.age) {
+            const current = this.pipe[0]
+
+            const branch = this.buildBranch(current)
+            this.blueprint.push(branch)
+
+            this.getNextInPipe(branch)
 
             this.drawn.value++
             this.pipe.shift()
         }
-
-        // déclencher la boucle d'animation
-        window.requestAnimationFrame(this.animate.bind(this))
     }
 
-    build(x, y, angle, depth, thickness, id, prevId) {
-        // Coordonnées théoriques du noeud suivant (endX, endY)
-        const endX = x + Math.cos(angle) * ((config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength))
-        const endY = y + Math.sin(angle) * ((config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength))
-
-        // Distance qui sépare le noeud actuel du noeud suivant
-        const length = Math.sqrt(Math.pow(endX - x, 2) + Math.pow(endY - y, 2))
-
-        // Spawn d'une feuille au noeud actuel
+    buildBranch({ id, prevId, x, y, angle, depth, thickness }) {
+        const { nextX, nextY } = this.nextCoordinates(x, y, angle)
+        const length = Math.sqrt(Math.pow(nextX - x, 2) + Math.pow(nextY - y, 2))
         const leafDice = config.seedRandom(x) - (depth - config.maxDepth) / 10
 
-        // Enregistrement du blueprint de la branche
-        this.blueprint.push({
+        return {
             id: id,
             prevId: prevId,
             depth: depth,
             startNode: { x: x, y: y },
-            endNode: { x: endX, y: endY },
-            color: colorWithVariation(config.branchColor, config.branchColorVariation, endX, config.seedRandom),
+            endNode: { x: nextX, y: nextY },
+            color: colorWithVariation(config.branchColor, config.branchColorVariation, nextX, config.seedRandom),
             length: length,
             thickness: thickness,
             angle: angle,
-            leaf: {dice: leafDice, ellipse: config.seedRandom(y) * 360, color: colorWithVariation(config.leafColor, config.leafColorVariation, x, config.seedRandom)},
-        })
+            leaf: { dice: leafDice, ellipse: config.seedRandom(y) * 360, color: colorWithVariation(config.leafColor, config.leafColorVariation, x, config.seedRandom) },
+        }
+    }
 
-        // Courbure des deux branches du noeud suivant
-        const d1 = getCurve(endX, endY, angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom)
-        const d2 = getCurve(endX + 1, endY + 1, angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom)
+    nextCoordinates(x, y, angle) {
+        const nextX = x + Math.cos(angle) * ((config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength))
+        const nextY = y + Math.sin(angle) * ((config.seedRandom(x, y) * (config.branchMaxLength - config.branchMinLength) + config.branchMinLength))
+
+        return { nextX: nextX, nextY: nextY }
+    }
+
+    getNextInPipe(branch) {
+        // TODO : check attended behavior
+        const d1 = getCurve(branch.endNode.x, branch.endNode.y, branch.angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom)
+        const d2 = getCurve(branch.endNode.x + 1, branch.endNode.y + 1, branch.angle, config.branchAngle, config.branchAngleVariation, config.branchMaxLength, config.branchMinLength, config.seedRandom)
 
         // Epaisseur des deux branches du noeud suivant
-        const thickness1 = getThickness(endY, thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom)
-        const thickness2 = getThickness(endY, thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom)
+        const thickness1 = getThickness(branch.endNode.y, branch.thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom)
+        const thickness2 = getThickness(branch.endNode.y, branch.thickness, config.branchThickness, config.branchThicknessVariation, config.seedRandom)
 
-        // Placement des deux branches suivantes sur la pile
+        this.pipe = this.pipe.concat(this.getNextInBlueprint(branch, d1, d2, thickness1, thickness2))
+    }
+
+    getNextInBlueprint(branch, d1, d2, thickness1, thickness2) {
         this.buildId++
-        this.pipe.push({ id: this.buildId, prevId: id, x: endX, y: endY, angle: angle + config.branchAngleVariation, length: length, depth: depth + 1, thickness: thickness1, ccpX: d1.x, ccpY: d1.y, leafDice: leafDice })
+        const firstNext = { id: this.buildId, prevId: branch.id, x: branch.endNode.x, y: branch.endNode.y, angle: branch.angle + config.branchAngleVariation, length: branch.length, depth: branch.depth + 1, thickness: thickness1, ccpX: d1.x, ccpY: d1.y, leafDice: branch.leaf.dice }
         this.buildId++
-        this.pipe.push({ id: this.buildId, prevId: id, x: endX, y: endY, angle: angle - config.branchAngleVariation, length: length, depth: depth + 1, thickness: thickness2, ccpX: d2.x, ccpY: d2.y, leafDice: leafDice })
+        const secondNext = { id: this.buildId, prevId: branch.id, x: branch.endNode.x, y: branch.endNode.y, angle: branch.angle - config.branchAngleVariation, length: branch.length, depth: branch.depth + 1, thickness: thickness2, ccpX: d2.x, ccpY: d2.y, leafDice: branch.leaf.dice }
+
+        return [firstNext, secondNext]
     }
 
     animate() {
+        window.requestAnimationFrame(this.drawAnimationFrame.bind(this))
+    }
+
+    drawAnimationFrame() {
         const now = window.performance.now()
         const delta = (now - this.lastFrameTime)
 
         if (delta > (1000 / this.fps)) {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-            
+
             const angleVariation = Math.random() * 1 / this.windResistance
+            const tree = this.buildAnimationTree(angleVariation)
 
-            let pipe = []
-            let framePipe = []
-
-            pipe.push(this.blueprint[0])
-
-            // enregistrer les branches à dessiner dans la frame
-            while (pipe.length > 0) {
-                const current = pipe[0]
-
-                // On calcule les coordonnées en fonction de la distance et de l'angle inscrits dans le blueprint
-                const endX = current.startNode.x + current.length * Math.cos((current.angle + (angleVariation * current.depth / 10))) // TODO : risk of failed behavior if depth >>> 10
-                const endY = current.startNode.y + current.length * Math.sin((current.angle + (angleVariation * current.depth / 10))) // TODO : risk of failed behavior if depth >>> 10
-
-                const previousBranch = framePipe.find(branch => branch.id === current.prevId)
-
-                framePipe.push({
-                    x: previousBranch ? previousBranch.endX : this.root.x,
-                    y: previousBranch ? previousBranch.endY : this.root.y,
-                    endX: endX,
-                    endY: endY,
-                    color: current.color,
-                    thickness: current.thickness,
-                    leaf: current.leaf,
-                    id: current.id,
-                })
-
-                const next = this.blueprint.find(branch => branch.id === current.id + 1)
-
-                if (next) {
-                    pipe.push(next)
-                }
-
-                pipe.shift()
-            }
-
-            // dessiner la frame
-            for (const branch of framePipe) {
+            for (const branch of tree) {
                 this.draw(branch.x, branch.y, branch.endX, branch.endY, branch.color, branch.thickness, branch.leaf)
             }
 
@@ -134,24 +142,49 @@ export class App {
         window.requestAnimationFrame(this.animate.bind(this))
     }
 
-    buildCanvas() {
-        this.canvas.width = config.canvasWidth
-        this.canvas.height = config.canvasHeight
-        this.container.appendChild(this.canvas)
+    buildAnimationTree(angleVariation) {
+        let pipe = []
+        let branches = []
 
-        this.context.fillStyle = config.skyColor
-        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        pipe.push(this.blueprint[0])
+
+        while (pipe.length > 0) {
+            const current = pipe[0]
+            const {endX, endY} = this.nextAnimatedCoordinates(current, angleVariation)
+            const previous = branches.find(branch => branch.id === current.prevId)
+
+            branches.push(this.getAnimationBranch(previous, current, endX, endY))
+
+            const next = this.blueprint.find(branch => branch.id === current.id + 1)
+
+            if (next) {
+                pipe.push(next)
+            }
+
+            pipe.shift()
+        }
+
+        return branches
     }
 
-    fillBackground() {
-        this.container.style.backgroundColor = config.skyColor
+    nextAnimatedCoordinates(blueprint, angleVariation) {
+        const endX = blueprint.startNode.x + blueprint.length * Math.cos((blueprint.angle + (angleVariation * blueprint.depth / 10))) // TODO : risk of failed behavior if depth >>> 10
+        const endY = blueprint.startNode.y + blueprint.length * Math.sin((blueprint.angle + (angleVariation * blueprint.depth / 10))) // TODO : risk of failed behavior if depth >>> 10
+        
+        return {endX, endY}
     }
 
-    writeInfo() {
-        this.info.innerHTML = `Cet arbre s'appelle ${config.seed.name}. Il pousse depuis ${config.age} jours.\n
-Pour le partager et le retrouver facilement, copiez le lien de cette page depuis la barre d'adresse de votre navigateur, ou ci-dessous :\n
-${window.location.href} \n\n
-L'accès à cet arbre est libre et gratuit pour tout le monde, et le restera pour toujours.`
+    getAnimationBranch(previous, current, endX, endY) {
+        return {
+            x: previous ? previous.endX : this.root.x,
+            y: previous ? previous.endY : this.root.y,
+            endX: endX,
+            endY: endY,
+            color: current.color,
+            thickness: current.thickness,
+            leaf: current.leaf,
+            id: current.id,
+        }
     }
 
     draw(x, y, endX, endY, color, thickness, leaf, curveControlPointX = x, curveControlPointY = y) {
